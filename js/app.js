9ea8376e634d07c059b3cdc68830e19798e11ee8
Erica Xu <erica.xu@uwaterloo.ca>
2015-02-16 12:13:08 -0500

    Trivial HTML fix.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var UI = function() {
 	this.$numLinesOfCode = $('#num-lines-of-code');
 	this.$numDollars = $('#num-dollars');
-}
+};
 
 UI.prototype.setLinesOfCode = function(newAmount) {
 	this.$numLinesOfCode.text(newAmount);
@@ -9,16 +9,21 @@ UI.prototype.setLinesOfCode = function(newAmount) {
 
 UI.prototype.setDollars = function(newAmount) {
 	this.$numDollars.text(newAmount);
-}
+};
+
+var ui = new UI();
 
 var GAME = new Game();
-GAME.AddResource(new Resource(GAME, "money"));
-GAME.AddResource(new Resource(GAME, "code"));
+GAME.AddResource(new Resource(GAME, 'money'));
+GAME.AddResource(new Resource(GAME, 'code'));
 
-GAME.AddGenerator(new Generator(GAME, "webdev").SetBaseRate("money", 1));
+GAME.AddGenerator(new Generator(GAME, 'webdev').SetBaseRate('money', 1).Add(1));
 
-GAME.events.on("post_loop", function(game) {
-	if (game.Every(10)) {
-		console.log(game.tick);
+GAME.events.on('post_loop', function(game) {
+	if (game.Every(25)) {
+		ui.setLinesOfCode(GAME.resources['code'].value);
+		ui.setDollars(GAME.resources['money'].value);
 	}
 });
+
+GAME.Start();
