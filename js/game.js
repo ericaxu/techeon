e23ac1449875559be67f892dcdf57c07eae24661
lishid <lishid@gmail.com>
2015-02-17 07:22:37 -0500

    moved events to game

--- js/game.js
+++ js/game.js
@@ -1,3 +1,37 @@
+
+var Events = function() {
+    this.events = {};
+};
+Events.prototype.on = function(name, func, context) {
+    if (!this.events[name]) {
+        this.events[name] = [];
+    }
+    this.events[name].push({func: func, context: context})
+};
+Events.prototype.off = function(name, func) {
+    if (!func || !this.events[name]) {
+        return;
+    }
+    var list = this.events[name];
+    for (var i = 0; i < list.length; i++) {
+        if (list[i] == func) {
+            list.splice(i, 1);
+        }
+    }
+};
+Events.prototype.trigger = function() {
+    var args = Array.apply([], arguments);
+    var name = args.shift();
+    var list = this.events[name];
+    if (!list) {
+        return;
+    }
+    for (var i = 0; i < list.length; i++) {
+        list[i].func.apply(list[i].context, args);
+    }
+};
+
+
 /**
  * Game
  */
